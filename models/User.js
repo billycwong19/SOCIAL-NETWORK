abd5ae3781e35592ac98b8c964002a7936398756
Ocelot <100012348+billycwong19@users.noreply.github.com>
2022-05-21 14:42:14 -0500

    working on thoughts model

--- models/User.js
+++ models/User.js
@@ -1,2 +1,36 @@
 const { Schema, model } = require('mongoose');
-const 
+const thoughtsSchema = require('./Thoughts')
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        max_length: 24,
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+            },
+            message: "Please enter a valid email"
+        },
+        required: [true, "Email required"]
+    },
+    thoughts: [thoughtsSchema],
+    friends: [userSchema]
+},
+{
+    toJSON: {
+        getters: true,
+    },
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
