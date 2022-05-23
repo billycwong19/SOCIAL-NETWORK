f827e5ab93f538d9012284212a51e55c6215114d
Ocelot <100012348+billycwong19@users.noreply.github.com>
2022-05-22 20:17:36 -0500

    thought routes completed and tested in insomnia

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -6,6 +6,11 @@ module.exports= {
             .then((thought) => res.json(thought))
             .catch((err) => res.status(500).json(err))
     },
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.id })
+            .then((thought) => res.json(thought))
+            .catch((err) => res.status(500).json('No a single thought found!'))
+    },
     createThought(req, res) {
         Thought.create(req.body)
           .then((thought) => {
@@ -20,12 +25,29 @@ module.exports= {
               ? res
                   .status(404)
                   .json({ message: 'Thought created, but found no user with that ID' })
-              : res.json('Created the thought!')
+              : res.json('Thought Created!')
           )
           .catch((err) => {
             console.log(err);
             res.status(500).json(err);
           });
-      },
+    },
+    updateThought(req, res) {
+      Thought.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: req.body.thoughtText },
+        { new: true })
+        .then((thought) => 
+            !thought ? res.status(404).json({ message: 'Not a single thought found!'})
+            : res.status(200).json({ message: 'Updated Thought!' }, thought))
+        .catch((err) => res.status(500).json(err))
+    },
+    deleteThought(req, res){
+      Thought.findOneAndDelete({ _id: req.params.id })
+      .then((user) => 
+            !user ? res.status(404).json({ message: 'Not a single thought found!'})
+            : res.json({ message: 'Thought has been deleted!'}))
+            .catch((err) => res.status(500).json(err))
+    }
     
 }
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,10 +1,15 @@
 const router = require('express').Router()
 
 const {
+    getAllThoughts,
+    getSingleThought,
     createThought,
-    getAllThoughts
+    updateThought,
+    deleteThought,
+    
 } = require('../../controllers/thoughtController')
 
 router.route('/').get(getAllThoughts).post(createThought)
+router.route('/:id').get(getSingleThought).put(updateThought).delete(deleteThought)
 
 module.exports = router;
